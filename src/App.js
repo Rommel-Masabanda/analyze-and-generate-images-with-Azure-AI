1e46b8065c8ad411d6d750b83ba725cb59f49e2c
Rommel Masabanda <rommel.masabanda@epn.edu.ec>
2023-11-11 23:52:11 -0500

    Improve style

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import './App.css';
 function App() {
   
   return <>
-  <title>Visión por computadora</title>
+  <h1>Visión por computadora</h1>
     <p>Utiliza este servicio para mejorar la usabilidad de tu sitio web, generamos textos descriptivos para tus imagenes atraves de Inteligencia Artificial</p>
-
+    <br/>
     <p>Ingresa la URL de tu imagen</p>
     <input type="text" name="name" />
     < button>Analizar</button>
