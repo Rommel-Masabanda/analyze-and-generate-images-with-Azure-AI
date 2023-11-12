74f54390fa27787baa4063bd30dd0a3afa84e239
Rommel Masabanda <rommel.masabanda@epn.edu.ec>
2023-11-11 23:40:10 -0500

    Add buttons and style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import './App.css';
+
 
 function App() {
   const value = 'World';
-  return <div>Hello amigos {value}</div>;
+  return <>
+  <title>Visión por computadora</title>
+    <p>Utiliza este servicio para mejorar la usabilidad de tu sitio web, generamos textos descriptivos para tus imagenes atraves de Inteligencia Artificial</p>
+
+    <p>Ingresa la URL de tu imagen</p>
+    <input type="text" name="name" />
+    < button>Analizar</button>
+    < button>Generar</button>
+
+  </>;
 
 }
 
