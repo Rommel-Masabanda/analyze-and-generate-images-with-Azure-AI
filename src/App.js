6a2ea7572ab9d12d0152755dc13020e94d667ee3
Rommel Masabanda <rommel.masabanda@epn.edu.ec>
2023-11-11 23:06:12 -0500

    Add buttons

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <>
+  <title>React App</title>
+    <div>Ingrese la url de la imagen</div>
+    <input type="text" />
+    <button>Analizar</button>
+    <button>Generar</button>
+  </>;
 }
 
 export default App;
